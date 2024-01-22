3faf3e43e945209ca961d73f957ca2c1bd2d8172
jiyue1214 <jiyue1214@gmail.com>
2024-01-22 13:54:03 +0000

    Update with the new meta-data yaml format

--- schema/metadata.py
+++ schema/metadata.py
@@ -32,37 +32,44 @@ Models
 
 
 class SampleMetadata(BaseModel):
+    sample_ancestry_category: Optional[List[str]] = None
+    sample_ancestry: Optional[List[str]] = None
+    sample_size: Optional[int] = None
     ancestry_method: Optional[List[str]] = None
     case_control_study: Optional[bool] = None
     case_count: Optional[int] = None
     control_count: Optional[int] = None
-    sample_size: int = None
-    sample_ancestry: List[str] = None
 
 
 class SumStatsMetadata(BaseModel):
-    genotyping_technology: List[str] = None
+    # Study meta-data
     gwas_id: Optional[constr(regex=r'^GCST\d+$')] = None
-    trait_description: List[str] = None
-    minor_allele_freq_lower_limit: Optional[float] = None
-    data_file_name: str = None
-    file_type: str = None
-    data_file_md5sum: str = None
-    is_harmonised: Optional[bool] = False
-    is_sorted: Optional[bool] = False
-    date_last_modified: date = None
-    genome_assembly: str = None
-    analysis_software: Optional[str] = None
-    imputation_panel: Optional[str] = None
-    imputation_software: Optional[str] = None
-    harmonisation_reference: Optional[str] = None
-    adjusted_covariates: Optional[List[str]] = None
-    ontology_mapping: Optional[List[str]] = None
     author_notes: Optional[str] = None
     gwas_catalog_api: Optional[str] = None
+    date_metadata_last_modified: date
+    # Trait Information
+    trait_description: Optional[List[str]] = None
+    ontology_mapping: Optional[List[str]] = None
+    # Genotyping Information
+    genome_assembly: str
+    coordinate_system: Optional[CoordinateSystemEnum] = None
+    genotyping_technology: Optional[List[str]] = None
+    imputation_panel: Optional[str] = None
+    imputation_software: Optional[str] = None
+    # Sample Information
+    samples: Optional[List[SampleMetadata]] = None
     sex: Optional[SexEnum] = None
-    coordinate_system: CoordinateSystemEnum = None
-    samples: List[SampleMetadata] = None
+    # Summary Statistic information:
+    data_file_name: str
+    file_type: str
+    data_file_md5sum: str
+    analysis_software: Optional[str] = None
+    adjusted_covariates: Optional[List[str]] = None
+    minor_allele_freq_lower_limit: Optional[float] = None
+    # Harmonization status
+    is_harmonised: Optional[bool] = False
+    is_sorted: Optional[bool] = False
+    harmonisation_reference: Optional[str] = None
     
     class Config:
         title = 'GWAS Summary Statistics metadata schema'
